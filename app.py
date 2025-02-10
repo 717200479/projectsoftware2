41915eb28cce101a6b30b116fc2760f4f8ea40fe
717200479 <ali.mobizy@gmail.com>
2025-02-10 11:30:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from controller import app
 from model import init_db
-
+print("hello")
 if __name__ == '__main__':
     init_db()  # Ensure the database is initialized before starting the app
     app.run(debug=True)
